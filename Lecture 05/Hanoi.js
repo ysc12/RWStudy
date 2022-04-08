2592a1a7a016484334c953defaed7f4551866796
세찬 유 <isr5012@daum.net>
2022-04-08 23:03:29 +0900

    [YSC] 6강 업로드

---
+++ Lecture 05/Hanoi.js
@@ -0,0 +1,6 @@
+ window.onload = function(){
+    document.querySelector('#btn').addEventListener('click', () => {
+        document.querySelector('#box1').classList.toggle('clicked')
+      })
+ }
+ 
